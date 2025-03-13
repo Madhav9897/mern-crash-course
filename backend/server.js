45950084d65793e71b29cf349598a1114a2993bf
Madhav9897 <madhavgoel131115@gmail.com>
2025-03-14 00:12:20 +0530

    updared package

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,12 @@ const __dirname = path.resolve();
 app.use(express.json()); //allows us to accept data in json
 app.use("/api/products", router);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+// }
 app.listen(portNumber, () => {
   connectDB();
   console.log("started yus");
